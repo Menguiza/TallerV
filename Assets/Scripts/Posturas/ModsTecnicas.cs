5b366ba941b6e4dcb0250235d3b6dc6ef71d059a
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-03-28 21:02:58 -0500

    Versión 3
    Esta implementación se realiza por medio de un SriptableObject Postura que contiene su correspondiente lista de ScriptableObject ModsTecnicas

--- Assets/Scripts/Posturas/ModsTecnicas.cs
+++ Assets/Scripts/Posturas/ModsTecnicas.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ModsTecnicas : MonoBehaviour
+[CreateAssetMenu(fileName = "NuevaTecnica", menuName = "ScriptableObjects/Tecnica", order = 1)]
+public class ModsTecnicas : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    //Los estados en los que se aplica la postura
+    enum ActiveState
     {
-        
+        awake,
+        normalDream,
+        nightmareDream,
+        anyDream
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Technique modifier values")]
+    [SerializeField] ActiveState activeState;
+    [SerializeField] string techniqueName;
+    [SerializeField] sbyte multVidaMax;
+    [SerializeField] sbyte multDmg;
+    [SerializeField] float multConciencia;
+    [SerializeField] sbyte multTGPC;
+    [SerializeField] sbyte multCritProb;
+    [SerializeField] float multCrit;
+    [SerializeField] sbyte multRoboPer;
+    [SerializeField] float multVelAatque;
+    [SerializeField] float multSpeed;
+    [SerializeField] sbyte multPesadillaPer;
 }
--- Assets/Scripts/Posturas/PosturaDelSueño.cs
+++ Assets/Scripts/Posturas/PosturaDelSueño.cs
@@ -7,29 +7,7 @@ public class PosturaDelSueño : ScriptableObject
 {
     [Header("Other")]
     [SerializeField] Sprite icon;
-    [Header("Sleep technique modifier")]
-    [SerializeField] string ST_name;
-    [SerializeField] sbyte ST_multVidaMax;
-    [SerializeField] sbyte ST_multDmg;
-    [SerializeField] float ST_multConciencia;
-    [SerializeField] sbyte ST_multTGPC;
-    [SerializeField] sbyte ST_multCritProb;
-    [SerializeField] float ST_multCrit;
-    [SerializeField] sbyte ST_multRoboPer;
-    [SerializeField] float ST_multVelAatque;
-    [SerializeField] float ST_multSpeed;
-    [SerializeField] sbyte ST_multPesadillaPer;
+    [Header("Sleep techniques")]
+    public List<ModsTecnicas> Techniques;
 
-    [Header("Dream technique modifier")]
-    [SerializeField] string DT_name;
-    [SerializeField] sbyte DT_multVidaMax;
-    [SerializeField] sbyte DT_multDmg;
-    [SerializeField] float DT_multConciencia;
-    [SerializeField] sbyte DT_multTGPC;
-    [SerializeField] sbyte DT_multCritProb;
-    [SerializeField] float DT_multCrit;
-    [SerializeField] sbyte DT_multRoboPer;
-    [SerializeField] float DT_multVelAatque;
-    [SerializeField] float DT_multSpeed;
-    [SerializeField] sbyte DT_multPesadillaPer;
 }
