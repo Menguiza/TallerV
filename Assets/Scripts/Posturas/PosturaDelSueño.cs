06b42652c40b25ded3ce8ae26f4d92a11d1ef786
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-03-28 20:32:31 -0500

    Version 2 del sistema de posturas
    Se implementará a forma de Scriptable object que estará en una lista

--- Assets/Scripts/Posturas/PosturaDelSueño.cs
+++ Assets/Scripts/Posturas/PosturaDelSueño.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PosturaDelSueño : MonoBehaviour
+[CreateAssetMenu(fileName = "NuevaPostura", menuName = "ScriptableObjects/Postura", order = 1)]
+public class PosturaDelSueño : ScriptableObject
 {
+    [Header("Other")]
+    [SerializeField] Sprite icon;
     [Header("Sleep technique modifier")]
     [SerializeField] string ST_name;
     [SerializeField] sbyte ST_multVidaMax;
@@ -29,78 +32,4 @@ public class PosturaDelSueño : MonoBehaviour
     [SerializeField] float DT_multVelAatque;
     [SerializeField] float DT_multSpeed;
     [SerializeField] sbyte DT_multPesadillaPer;
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Apply_SleepTechnique();
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Remove_SleepTechnique();
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Apply_DreamTechnique();
-        }
-
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            Remove_DreamTechnique();
-        }
-    }
-
-    void AssertGameMasterReference()
-    {
-        if (transform.parent.gameObject.TryGetComponent<GameMaster>(out GameMaster gameMaster) == false)
-        {
-            Debug.LogWarning("|POSTURA DEL SUEÑO| No se logró acceder al GameMaster");
-            return;
-        }
-    }
-
-    public void Apply_SleepTechnique()
-    {
-        AssertGameMasterReference();
-        GetComponentInParent<GameMaster>().AddMod(ST_name, ST_multVidaMax, ST_multDmg, ST_multConciencia, ST_multTGPC, ST_multCritProb, ST_multCrit, ST_multRoboPer, ST_multVelAatque, ST_multSpeed, ST_multPesadillaPer);
-    }
-
-    public void Remove_SleepTechnique()
-    {
-        AssertGameMasterReference();
-        foreach (Mods mod in GetComponentInParent<GameMaster>().mods)
-        {
-            if (mod.Name == ST_name)
-            {
-                GetComponentInParent<GameMaster>().mods.Remove(mod);
-                GetComponentInParent<GameMaster>().CheckMods();
-                return;
-            }
-        }
-        Debug.LogWarning("|POSTURA DEL SUEÑO| No se encontró el Modificador de la Técnica de dormir " + ST_name + " a remover");
-    }
-
-    public void Apply_DreamTechnique()
-    {
-        AssertGameMasterReference();
-        GetComponentInParent<GameMaster>().AddMod(DT_name, DT_multVidaMax, DT_multDmg, DT_multConciencia, DT_multTGPC, DT_multCritProb, DT_multCrit, DT_multRoboPer, DT_multVelAatque, DT_multSpeed, DT_multPesadillaPer);
-    }
-
-    public void Remove_DreamTechnique()
-    {
-        AssertGameMasterReference();
-        foreach (Mods mod in GetComponentInParent<GameMaster>().mods)
-        {
-            if (mod.Name == DT_name)
-            {
-                GetComponentInParent<GameMaster>().mods.Remove(mod);
-                GetComponentInParent<GameMaster>().CheckMods();
-                return;
-            }
-        }
-        Debug.LogWarning("|POSTURA DEL SUEÑO| No se encontró el Modificador de la Técnica del sueño " + DT_name + " a remover");
-    }
 }
