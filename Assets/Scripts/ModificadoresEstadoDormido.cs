d527c19ed3e2a9e07d9179aefc605aee96da19db
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-04-01 05:47:57 -0500

    Agregar mod estado soñando

---
+++ Assets/Scripts/ModificadoresEstadoDormido.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModificadoresEstadoDormido : MonoBehaviour
+{
+    //Modificar para el estado pesadilla, falta agregar
+
+    public void AddDreamModifier()
+    {
+        GameMaster.instance.AddMod("Arte del soñador", 0, 5, 0, 0, 0, 0, 0, 0, 0, 0);
+    }
+
+    public void RemoveDreamModifier()
+    {
+        GameMaster.instance.RemoveMod("Arte del soñador");
+    }
+}
