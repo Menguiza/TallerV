1846537669318df8ab16d612dfa93d1f8daa307f
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-04-01 04:29:26 -0500

    Integración con URP y VFX

--- Assets/Scripts/Hechizos/BolaDeFuego/Proyectil_BolaDeFuego.cs
+++ Assets/Scripts/Hechizos/BolaDeFuego/Proyectil_BolaDeFuego.cs
@@ -16,9 +16,6 @@ public class Proyectil_BolaDeFuego : MonoBehaviour
         //Aqui van las particulas --
 
         //Separar el trail
-        Destroy(gameObject.transform.GetChild(0).gameObject, 1f);
-        
-        gameObject.transform.DetachChildren();
 
         //Siempre Eliminar al final el proyectil
         Destroy(gameObject);
