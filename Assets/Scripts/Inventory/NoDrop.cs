e5e02bc9a8287668fb78b9c00bf352e1881a0e08
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-04-01 19:37:22 -0500

    Nojado por no mostrar el DragNDrop

--- Assets/Scripts/Inventory/NoDrop.cs
+++ Assets/Scripts/Inventory/NoDrop.cs
@@ -7,6 +7,12 @@ public class NoDrop : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
-        eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = eventData.pointerDrag.GetComponent<DragDrop>().position;
+        if(eventData.pointerDrag != null)
+        {
+            if(eventData.pointerDrag.GetComponent<RectTransform>() != null)
+            {
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = eventData.pointerDrag.GetComponent<DragDrop>().position;
+            }
+        }  
     }
 }
