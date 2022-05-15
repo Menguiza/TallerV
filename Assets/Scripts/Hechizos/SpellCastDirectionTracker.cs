d35429aec77c0a6a50f2d05172cde6a1d6141ac7
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-05-15 12:34:17 -0500

    Ajustes al ataque básico y hechizos (animaciones)

--- Assets/Scripts/Hechizos/SpellCastDirectionTracker.cs
+++ Assets/Scripts/Hechizos/SpellCastDirectionTracker.cs
@@ -6,11 +6,16 @@ public class SpellCastDirectionTracker : MonoBehaviour
 {
     Vector3 screenPos;
     Vector3 worldPos;
-    [SerializeField] Transform refObject;
 
     private void Start()
     {
         screenPos.z = Vector3.Distance(GameMaster.instance.playerObject.transform.position, Camera.main.transform.position);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(worldPos, 0.5f);
     }
 
     void Update()
@@ -18,7 +23,6 @@ public class SpellCastDirectionTracker : MonoBehaviour
         screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPos.z);
         
         worldPos = Camera.main.ScreenToWorldPoint(screenPos);
-        refObject.position = worldPos;
 
         print("SP (X:" + screenPos.x + " Y:" + screenPos.y + ") WP (X:" + worldPos.x + " Y:" + worldPos.y + " Z:" + worldPos.z + ")");
     }
