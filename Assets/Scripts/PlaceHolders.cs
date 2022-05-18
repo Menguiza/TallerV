7d5b576a2fb67a2cfbec2c7feff74db198b60613
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-05-18 07:26:23 -0500

    Se supone que funciona, pero como no inicializamos los singletons F

---
+++ Assets/Scripts/PlaceHolders.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlaceHolders : MonoBehaviour
+{
+    public Sprite icon = null;
+
+    public TMP_Text coins, gems;
+
+    [SerializeField]
+    Image img;
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.sprite = icon;
+
+        transform.position = GameMaster.instance.playerObject.transform.position;
+    }
+}
