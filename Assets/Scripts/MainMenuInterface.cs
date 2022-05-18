b8b5b43e0650a907d287da2aa92a6a841849d5e9
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2022-05-18 02:12:50 -0500

    Ajustes lobby y b1 tienda

--- Assets/Scripts/MainMenuInterface.cs
+++ Assets/Scripts/MainMenuInterface.cs
@@ -15,6 +15,54 @@ public class MainMenuInterface : MonoBehaviour
         if (File.Exists(Application.persistentDataPath + "/DwizardSave.dat")) continueButton.interactable = true;
         else continueButton.interactable = false;
         DontDestroyOnLoad(gameObject);
+
+        #region"Clean singletons"
+        GameMaster gm = GameMaster.instance;
+        Economy ec = Economy.instance;
+        Inventory inv = Inventory.instance;
+        ManagerHechizos mh = ManagerHechizos.instance;
+        SoundManager sm = SoundManager.instance;
+        RoomManager rm = RoomManager.instance;
+
+        if (gm != null)
+        {
+            gm.StopAllCoroutines();
+            gm.CancelInvoke();
+            Destroy(gm.gameObject);
+            
+        }
+        if (ec != null)
+        {
+            ec.StopAllCoroutines();
+            ec.CancelInvoke();
+            Destroy(ec.gameObject);
+        }
+        if (inv != null)
+        {
+            inv.StopAllCoroutines();
+            inv.CancelInvoke();
+            Destroy(inv.gameObject);
+        }
+        if (mh != null)
+        {
+            mh.StopAllCoroutines();
+            mh.CancelInvoke();
+            Destroy(mh.gameObject);
+        }
+        if (sm != null)
+        {
+            sm.StopAllCoroutines();
+            sm.CancelInvoke();
+            Destroy(sm.gameObject);
+        }
+        if (rm != null)
+        {
+            rm.StopAllCoroutines();
+            rm.CancelInvoke();
+            Destroy(rm.gameObject);
+        }
+        #endregion
+
     }
 
     private void Update()
