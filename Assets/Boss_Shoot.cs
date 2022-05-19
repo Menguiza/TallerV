dc4f5ee3e9f290739cded83699fc58b810582043
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2022-05-19 12:49:25 -0500

    Ultimito

--- Assets/Boss_Shoot.cs
+++ Assets/Boss_Shoot.cs
@@ -11,7 +11,7 @@ public class Boss_Shoot : StateMachineBehaviour
     public float timeToSpawn, speed = 4f, timer;
     int count = 0;
 
-    bool change = false;
+    bool change = false, chase = false;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -21,6 +21,7 @@ public class Boss_Shoot : StateMachineBehaviour
         boss = animator.transform.parent.GetComponent<Boss>();
         timer = 0;
         change = false;
+        chase = false;
         count = 0;
     }
 
@@ -32,11 +33,28 @@ public class Boss_Shoot : StateMachineBehaviour
             return;
         }
 
-        Vector3 target = new Vector3(rb.position.x, boss.altitude, player.position.z + 3);
-        Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
-        rb.MovePosition(newPos);
+        if (chase)
+        {
+            Vector3 target = new Vector3(rb.position.x, rb.position.y, player.position.z + 3);
+            Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+            rb.MovePosition(newPos);
 
-        boss.LookAtPlayer(player.position);
+            boss.LookAtPlayer(player.position);
+        }
+
+        if(!chase)
+        {
+            Vector3 target = new Vector3(rb.position.x, boss.altitude, rb.position.z);
+            Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+            rb.MovePosition(newPos);
+
+            boss.LookAtPlayer(target);
+        }
+
+        if (rb.position.y == boss.altitude && !chase)
+        {
+            chase = true;
+        }
 
         if (count < 5)
         {
